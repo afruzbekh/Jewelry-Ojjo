0d8fbe217133a54bda11ce949faac8122b4c951d
afruzbekh <afruzbek1595@gmail.com>
2024-06-21 09:52:46 +0500

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 120;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+const burgerBtn = document.getElementById("burgerBtn");
+const mobile = document.getElementById("mobile");
+
+burgerBtn.addEventListener("click", function () {
+  mobile.classList.toggle("navigation");
+});
